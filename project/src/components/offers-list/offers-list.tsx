cba61caabe18ba86e601748a1aba20ecfe24bb8d
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-02-27 18:52:15 +0300

    fix(offers-list): add style none to list

--- project/src/components/offers-list/offers-list.tsx
+++ project/src/components/offers-list/offers-list.tsx
@@ -10,7 +10,7 @@ export function OffersList({ offers }: OffersListProps): JSX.Element {
   const [activeCard, setActiveCard] = useState<number>();
 
   return (
-    <ul className="cities__places-list places__list tabs__content">
+    <ul className="cities__places-list places__list tabs__content" style={{listStyle: 'none'}}>
       {offers.map((offer) => {
         const { id } = offer;
         return <li key={id} onMouseEnter={() => { setActiveCard(id); }}>{activeCard}<PlaceCard {...offer} /></li>;
