7a91d654231221ccb0bd181455e705089a0112b7
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-03-08 14:38:22 +0300

    fix(favorite-place-card): change bookmark view

--- project/src/components/favorite-place-card/favorite-place-card.tsx
+++ project/src/components/favorite-place-card/favorite-place-card.tsx
@@ -3,8 +3,6 @@ import { Offer } from '../app/app';
 import { RatingStars } from '../rating-stars/rating-stars';
 
 export function FavoritePlaceCard({ id, isPremium, isFavorite, previewImage, price, type, title, rating }: Offer): JSX.Element {
-  const bookmarkClassName = isFavorite ? 'place-card__bookmark-button--active' : '';
-
   return (
     <article className="favorites__card place-card">
       {isPremium &&
@@ -22,12 +20,21 @@ export function FavoritePlaceCard({ id, isPremium, isFavorite, previewImage, pri
             <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className={`place-card__bookmark-button ${bookmarkClassName} button`} type="button">
-            <svg className="place-card__bookmark-icon" width="18" height="19">
-              <use xlinkHref="#icon-bookmark"></use>
-            </svg>
-            <span className="visually-hidden">In bookmarks</span>
-          </button>
+          {isFavorite ? (
+            <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
+              <svg className="place-card__bookmark-icon" width="18" height="19">
+                <use xlinkHref="#icon-bookmark"></use>
+              </svg>
+              <span className="visually-hidden">In bookmarks</span>
+            </button>
+          ) : (
+            <button className="place-card__bookmark-button button" type="button">
+              <svg className="place-card__bookmark-icon" width="18" height="19">
+                <use xlinkHref="#icon-bookmark"></use>
+              </svg>
+              <span className="visually-hidden">To bookmarks</span>
+            </button>
+          )}
         </div>
         <RatingStars rating={rating} className='place-card' />
         <h2 className="place-card__name">
