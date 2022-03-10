50b7e0c175354d1074a71aa48073ee3389d9e1b1
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-03-10 08:11:34 +0300

    feat: add provider to app

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,14 +1,13 @@
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App} from './components/app/app';
-import { OFFERS } from './mocks/offers';
-import { REVIEWS } from './mocks/reviews';
+import { App } from './components/app/app';
+import { store } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      offers={OFFERS}
-      reviews={REVIEWS}
-    />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
