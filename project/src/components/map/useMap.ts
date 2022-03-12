f48cf923625c38981176dfc701f91c50399866a5
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-03-12 13:31:22 +0300

    fix: change map update

--- project/src/components/map/useMap.ts
+++ project/src/components/map/useMap.ts
@@ -15,25 +15,35 @@ export function useMap(
 ): Map | null {
   const [map, setMap] = useState<Map | null>(null);
   useEffect(() => {
-    if (mapRef.current !== null && map === null) {
-      const instance = new Map(mapRef.current, {
-        center: {
-          lat: city.location.latitude,
-          lng: city.location.longitude,
-        },
-        zoom: city.location.zoom,
-      });
-
-      const layer = new TileLayer(
-        'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
-        {
-          attribution:
-            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        },
-      );
-      instance.addLayer(layer);
-      setMap(instance);
+    if (!mapRef.current) {
+      return;
     }
+
+    if (map) {
+      map?.setView({
+        lat: city.location.latitude,
+        lng: city.location.longitude,
+      });
+      return;
+    }
+
+    const instance = new Map(mapRef.current, {
+      center: {
+        lat: city.location.latitude,
+        lng: city.location.longitude,
+      },
+      zoom: city.location.zoom,
+    });
+
+    const layer = new TileLayer(
+      'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      },
+    );
+    instance.addLayer(layer);
+    setMap(instance);
   }, [map, mapRef, city]);
   return map;
 }
