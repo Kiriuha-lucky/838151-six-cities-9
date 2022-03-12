1b07388ef9b0f86c0c824240971289b54c802f66
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-03-12 13:41:13 +0300

    fix(reducer): add initialStateType

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -3,13 +3,22 @@ import { OFFERS } from '../mocks/offers';
 import { REVIEWS } from '../mocks/reviews';
 import { CITIES } from '../types/cities';
 import { getCurrentCity, selectedOfferId } from './action';
+import {Offer, ReviewType} from '../components/app/app';
 
-const initialState = {
+interface initialStateType {
+  currentCity: string,
+  cities: string[],
+  offers: Offer[],
+  reviews: ReviewType[],
+  selectedOfferId: number
+}
+
+const initialState: initialStateType = {
   currentCity: 'Paris',
   cities: CITIES,
   offers: OFFERS,
   reviews: REVIEWS,
-  selectedOfferId: '',
+  selectedOfferId: 0,
 };
 
 export const reducer = createReducer(initialState, (builder) => {
