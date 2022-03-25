a120659ccb503936b4d78101395f137cc36d1d9e
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-03-25 12:29:14 +0300

    fix(offers-sort): add handleSortClick function

--- project/src/components/offers-sort/offers-sort.tsx
+++ project/src/components/offers-sort/offers-sort.tsx
@@ -8,9 +8,13 @@ export function OffersSort(): JSX.Element {
   const [open, setOpen] = useState(false);
 
   const sortClassName = open ? 'places__options--opened' : '';
-  const arrowClassName = open ? 'places__sorting-arrow places__sorting-arrow--open' : 'places__sorting-arrow ';
+  const arrowClassName = open ? 'places__sorting-arrow places__sorting-arrow--open' : 'places__sorting-arrow';
   const dispatch = useAppDispatch();
   const offersSortActive = useAppSelector((state) => state.offersSort);
+
+  function handleSortClick(sortType: string) {
+    dispatch(offersSort(sortType));
+  }
 
   return (
     <form className="places__sorting" action="#" method="get" onClick={() => setOpen(!open)}>
@@ -22,7 +26,7 @@ export function OffersSort(): JSX.Element {
         </svg>
       </span>
       <ul className={`places__options places__options--custom ${sortClassName}`} >
-        {SORT_TYPES.map((sortType) => <li key={sortType} className={`places__option ${sortType === offersSortActive ? 'places__option--active' : ''}`} tabIndex={0} onClick={() => { dispatch(offersSort(sortType)); }}>{sortType}</li>)}
+        {SORT_TYPES.map((sortType) => <li key={sortType} className={`places__option ${sortType === offersSortActive ? 'places__option--active' : ''}`} tabIndex={0} onClick={() => handleSortClick(sortType)}>{sortType}</li>)}
       </ul>
     </form>
   );
