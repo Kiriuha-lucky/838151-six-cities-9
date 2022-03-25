8fbf415ff8ea41790859d35e72b8492ebd32065e
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-03-25 15:17:02 +0300

    fix(action): add action type

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,4 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
+import { Offer } from '../components/app/app';
+import { initialStateType } from './reducer';
 
 export const Action = {
   GET_CURRENT_CITY: 'GET_CURRENT_CITY',
@@ -8,17 +10,17 @@ export const Action = {
   DATA_LOADED: 'DATA_LOADED',
 };
 
-export const getCurrentCity = createAction(Action.GET_CURRENT_CITY, (value) => ({ payload: value }),
+export const getCurrentCity = createAction(Action.GET_CURRENT_CITY, (value: initialStateType['currentCity']) => ({ payload: value }),
 );
 
-export const selectedOfferId = createAction(Action.SELECTED_OFFER_ID, (value) => ({ payload: value }),
+export const selectedOfferId = createAction(Action.SELECTED_OFFER_ID, (value: initialStateType['selectedOfferId']) => ({ payload: value }),
 );
 
-export const offersSort = createAction(Action.OFFERS_SORT, (value) => ({ payload: value }),
+export const offersSort = createAction(Action.OFFERS_SORT, (value: initialStateType['offersSortingType']) => ({ payload: value }),
 );
 
-export const loadOffers = createAction(Action.LOAD_OFFERS, (value) => ({ payload: value }),
+export const loadOffers = createAction(Action.LOAD_OFFERS, (value: Offer[]) => ({ payload: value }),
 );
 
-export const dataLoaded = createAction(Action.DATA_LOADED, (value) => ({ payload: value }),
+export const dataLoaded = createAction(Action.DATA_LOADED, (value: initialStateType['isDataLoaded']) => ({ payload: value }),
 );
