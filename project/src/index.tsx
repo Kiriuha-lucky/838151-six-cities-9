8bdfbd682d84cbaaf8a418aeffcfa64698ce6958
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-03-26 16:31:18 +0300

    feat: add toastify container to index

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,13 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/app/app';
 import { store } from './store';
-import {fetchOffersAction} from './store/api-actions';
+import { requireAuthAction } from './store/api-actions';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-store.dispatch(fetchOffersAction());
+
+store.dispatch(requireAuthAction());
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ToastContainer />
       <App />
     </Provider>
   </React.StrictMode>,
