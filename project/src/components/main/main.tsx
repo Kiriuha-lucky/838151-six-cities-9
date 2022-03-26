4663af1d580e84587a314f4c1776caae038b28f4
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-03-26 16:35:09 +0300

    feat(main): add header component

--- project/src/components/main/main.tsx
+++ project/src/components/main/main.tsx
@@ -1,52 +1,44 @@
-import { Offer } from '../app/app';
-import { Link } from 'react-router-dom';
+/*eslint-disable*/
+import { Offer } from '../../types/offer.types';
 import { OffersList } from '../offers-list/offers-list';
 import { Map } from '../map/map';
-import { useAppSelector } from '../../hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import { CitiesList } from '../cities-list/cities-list';
 import { OffersSort } from '../offers-sort/offers-sort';
 import { getCurrentOffers } from './main.utils';
+import { Header } from '../header/header';
+import { fetchOffersAction } from '../../store/api-actions';
+import { useEffect } from 'react';
+import { AuthorizationStatus } from '../../types/authorization.types';
+import { Spinner } from '../spinner/spinner';
 
 interface MainProps {
   offers: Offer[],
 }
 
 export function Main({ offers }: MainProps): JSX.Element {
+  const dispatch = useAppDispatch();
+  const { authorizationStatus, isDataLoaded } = useAppSelector((st) => st);
+
+  useEffect(() => {
+    dispatch(fetchOffersAction());
+  }, []);
+
   const currentCity = useAppSelector((state) => state.currentCity);
   const cities = useAppSelector((state) => state.cities);
   const offersSortingType = useAppSelector((state) => state.offersSortingType);
 
   const currentOffers = getCurrentOffers(offers, currentCity, offersSortingType);
+
+  if (authorizationStatus === AuthorizationStatus.Unknown || !isDataLoaded) {
+    return (
+      <Spinner />
+    );
+  }
+
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Link className="header__logo-link header__logo-link--active" to="/">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </Link>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="mailto:Oliver.conner@gmail.com">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="/">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+      <Header />
       <main className={`page__main page__main--index ${!currentOffers.length ? 'page__main--index-empty' : ''}`}>
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
