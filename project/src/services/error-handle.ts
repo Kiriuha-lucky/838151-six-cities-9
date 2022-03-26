cd1224c3643e8623c99161efdee414cfcb6d7a31
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-03-26 16:29:14 +0300

    feat: add error-handle component with tostify

---
+++ project/src/services/error-handle.ts
@@ -0,0 +1,25 @@
+import request from 'axios';
+import { toast } from 'react-toastify';
+import { HTTP_CODE } from '../types/http-code';
+
+export const errorHandle = (error: unknown): void => {
+  if (!request.isAxiosError(error)) {
+    throw error;
+  }
+
+  const { response } = error;
+
+  if (response) {
+    switch (response.status) {
+      case HTTP_CODE.BAD_REQUEST:
+        toast.error(response.data.error);
+        break;
+      case HTTP_CODE.UNAUTHORIZED:
+        toast.error(response.data.error);
+        break;
+      case HTTP_CODE.NOT_FOUND:
+        toast.error(response.data.error);
+        break;
+    }
+  }
+};
