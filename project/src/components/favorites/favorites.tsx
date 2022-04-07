27f9864bd7d003c95b7d7931ee3749b3a4fd939d
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-04-07 20:51:19 +0300

    feat(favorites): add empty page view

--- project/src/components/favorites/favorites.tsx
+++ project/src/components/favorites/favorites.tsx
@@ -8,7 +8,7 @@ import { Spinner } from '../spinner/spinner';
 export function Favorites(): JSX.Element {
   const dispatch = useAppDispatch();
   const [isDataLoaded, setIsDataLoaded] = useState(false);
-  const favoritesOffers = Object.values(useAppSelector(({favoritesOffersList}) => favoritesOffersList));
+  const favoritesOffers = Object.values(useAppSelector(({ favoritesOffersList }) => favoritesOffersList));
 
   const fetchData = async () => {
     await dispatch(fetchFavoritesOffersAction());
@@ -64,12 +64,23 @@ export function Favorites(): JSX.Element {
 
       <main className="page__main page__main--favorites">
         <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              {cityNames.map((city) => <FavoriteLocationItem key={city} cityName={city} offers={favoritesOffers.filter((offer) => offer.city.name === city)} />)}
-            </ul>
-          </section>
+          {favoritesOffers.length ?
+            (
+              <section className="favorites">
+                <h1 className="favorites__title">Saved listing</h1>
+                <ul className="favorites__list">
+                  {cityNames.map((city) => <FavoriteLocationItem key={city} cityName={city} offers={favoritesOffers.filter((offer) => offer.city.name === city)} />)}
+                </ul>
+              </section>
+            ) : (
+              <section className="favorites favorites--empty">
+                <h1 className="visually-hidden">Favorites (empty)</h1>
+                <div className="favorites__status-wrapper">
+                  <b className="favorites__status">Nothing yet saved.</b>
+                  <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+                </div>
+              </section>
+            )}
         </div>
       </main>
       <footer className="footer container">
