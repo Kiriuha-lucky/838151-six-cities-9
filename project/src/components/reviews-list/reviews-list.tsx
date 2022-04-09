ce4660b477948256689335a9316fc4c83f3780bd
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-04-09 11:57:03 +0300

    feat: add reviews sort by date

--- project/src/components/reviews-list/reviews-list.tsx
+++ project/src/components/reviews-list/reviews-list.tsx
@@ -4,13 +4,13 @@ import { Review } from '../review/review';
 interface ReviewsListProps {
   reviews: ReviewType[]
 }
-
-export function ReviewsList({reviews}: ReviewsListProps): JSX.Element {
+/*eslint-disable*/
+export function ReviewsList({ reviews }: ReviewsListProps): JSX.Element {
   const MAX_RIVIEWS_ON_PAGE = 10;
 
   return (
     <ul className="reviews__list">
-      {reviews.slice(0, MAX_RIVIEWS_ON_PAGE).map((review) => (<li key={review.id} className="reviews__item"><Review {...review} /></li>))}
+      {reviews.slice(0, MAX_RIVIEWS_ON_PAGE).sort((a: ReviewType, b: ReviewType) => Date.parse(b.date) - Date.parse(a.date)).map((review) => (<li key={review.id} className="reviews__item"><Review {...review} /></li>))}
     </ul>
   );
 }
