020811f6dbec1b8f959c703dc8f259f97c191fcb
Kiriuha-lucky <Kiriuha777@gmail.com>
2022-04-10 13:23:03 +0300

    fix: delete comment code

--- project/src/store/index.ts
+++ project/src/store/index.ts
@@ -1,21 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createAPI } from '../services/api';
-// import { AuthorizationStatus } from '../types/authorization.types';
-// import { Offer } from '../types/offer.types';
-// import { OffersSortingType, Property } from '../types/state';
 import { rootReducer } from './root-reducer';
 
 export const api = createAPI();
-
-// export interface State {
-//   offersList: Offer[],
-//   property: Property,
-//   auth: AuthorizationStatus,
-//   activeOffer: number,
-//   sort: OffersSortingType,
-//   currentCity: string,
-//   favoritesOffersList: Offer[]
-// }
 
 export const store = configureStore({
   reducer: rootReducer,
